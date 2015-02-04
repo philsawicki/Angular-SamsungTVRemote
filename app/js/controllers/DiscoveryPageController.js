6a77ede8de5a17bdb1cfdf4304b26c181e8fa6eb
Philippe Sawicki <philsawicki@users.noreply.github.com>
2015-02-03 21:38:37 -0500

    Adds UPnP Discovery page
    Adds base structure for a UPnP Device & Service Discovery page.

--- app/js/controllers/DiscoveryPageController.js
+++ app/js/controllers/DiscoveryPageController.js
@@ -1,9 +1,29 @@
 'use strict';
 
 /**
- * Discovery Page Controller
+ * Discovery Page Controller.
  */
 angular.module('smartTVRemote.Controllers')
-	.controller('DiscoveryPageController', ['$scope', function ($scope) {
-		
+	.controller('DiscoveryPageController', ['$scope', 'discoveryService', function ($scope, discoveryService) {
+		// Get SmartTVs connected through UPnP:
+		var smartTVsPromise = discoveryService.getConnectedSamsungSmartTVs();
+		smartTVsPromise.then(
+			function success (data) {
+				$scope.tvDevices = data;
+			},
+			function error (reason) {
+				console.error(reason);
+			}
+		);
+
+		// Get all devices connected through UPnP:
+		var devicesPromise = discoveryService.getConnectedDevices();
+		devicesPromise.then(
+			function success (data) {
+				$scope.devices = data;
+			},
+			function error (reason) {
+				console.error(reason);
+			}
+		);
 	}]);
---
+++ app/js/services/DiscoveryService.js
@@ -0,0 +1,141 @@
+'use strict';
+
+/**
+ * UPnP Discovery Service.
+ */
+angular.module('smartTVRemote.Services')
+	.factory('discoveryService', ['$q', '$http', '$timeout', 'appConfig', 'errorMessages', 
+		function ($q, $http, $timeout, appConfig, errorMessages) {
+
+			/**
+			 * Gets the Samsung SmartTVs discovered on the network through UPnP.
+			 * @return {Deferred.promise} A promise to be resolved when the request is successfully received.
+			 */
+			var getConnectedSamsungSmartTVs = function () {
+				var deferred = $q.defer();
+				var timeoutPromise = $q.defer();
+				var requestTimedOut = false;
+				var timeoutCountdown = undefined;
+				
+				var url = '//localhost:8080/api/tv/discovery';
+				
+				$http.get(url, { timeout: timeoutPromise.promise })
+					.success(function (data) {
+						if (data.length === 0) {
+							// Fail the request, as no data has been received:
+							deferred.reject({
+								error: errorMessages.NoData.Error,
+								message: errorMessages.NoData.Message
+							});
+						} else {
+							// Parse & format the data received:
+							for (var i = 0, count = data.length; i < count; i++) {
+								var deviceInfo = data[i];
+
+								// Split on line breaks ("/r/n"):
+								deviceInfo.headersFormatted = deviceInfo.headers.split( String.fromCharCode(13, 10) );
+							}
+						}
+						
+
+						// Cancel the "timeout" $timeout:
+						$timeout.cancel(timeoutCountdown);
+						// Cancel the "timeout" Promise:
+						timeoutPromise.reject();
+
+						
+						// Resolve the Promise with data:
+						deferred.resolve(data);
+					})
+					.error(function (data) {
+						if (requestTimedOut) {
+							deferred.reject({
+								error: errorMessages.Timeout.Error,
+								message: errorMessages.Timeout.Message.format(appConfig.JSONPTimeout),
+								data: data
+							});
+						} else {
+							deferred.reject(data);
+						}
+					});
+
+
+				// Start a $timeout which, if resolved, will fail the $http request sent (and assume a timeout):
+				timeoutCountdown = $timeout(function () {
+					requestTimedOut = true;
+					timeoutPromise.resolve();
+				}, appConfig.JSONPTimeout);
+
+				return deferred.promise;
+			};
+
+			/**
+			 * Gets all the devices discovered on the network through UPnP.
+			 * @return {Deferred.promise} A promise to be resolved when the request is successfully received.
+			 */
+			var getConnectedDevices = function () {
+				var deferred = $q.defer();
+				var timeoutPromise = $q.defer();
+				var requestTimedOut = false;
+				var timeoutCountdown = undefined;
+				
+				var url = '//localhost:8080/api/discovery/all';
+				
+				$http.get(url, { timeout: timeoutPromise.promise })
+					.success(function (data) {
+						if (data.length === 0) {
+							// Fail the request, as no data has been received:
+							deferred.reject({
+								error: errorMessages.NoData.Error,
+								message: errorMessages.NoData.Message
+							});
+						} else {
+							// Parse & format the data received:
+							for (var i = 0, count = data.length; i < count; i++) {
+								var deviceInfo = data[i];
+
+								// Split on line breaks ("/r/n"):
+								deviceInfo.headersFormatted = deviceInfo.headers.split( String.fromCharCode(13, 10) );
+							}
+						}
+						
+
+						// Cancel the "timeout" $timeout:
+						$timeout.cancel(timeoutCountdown);
+						// Cancel the "timeout" Promise:
+						timeoutPromise.reject();
+
+						
+						// Resolve the Promise with data:
+						deferred.resolve(data);
+					})
+					.error(function (data) {
+						if (requestTimedOut) {
+							deferred.reject({
+								error: errorMessages.Timeout.Error,
+								message: errorMessages.Timeout.Message.format(appConfig.JSONPTimeout),
+								data: data
+							});
+						} else {
+							deferred.reject(data);
+						}
+					});
+
+
+				// Start a $timeout which, if resolved, will fail the $http request sent (and assume a timeout):
+				timeoutCountdown = $timeout(function () {
+					requestTimedOut = true;
+					timeoutPromise.resolve();
+				}, appConfig.JSONPTimeout);
+
+				return deferred.promise;
+			};
+			
+
+			// Return the public interface for the service:
+			return {
+				getConnectedSamsungSmartTVs: getConnectedSamsungSmartTVs,
+				getConnectedDevices: getConnectedDevices
+			};
+		}
+	]);
